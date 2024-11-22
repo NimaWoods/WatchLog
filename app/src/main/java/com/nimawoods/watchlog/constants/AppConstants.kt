6bd00ad9f7447c17cf1195dd92c7c8c276dd3a96
Nima Woods <niklasfabritzius22@gmail.com>
2024-11-22 06:47:09 +0100

    Added Constants (Way to late)

---
+++ app/src/main/java/com/nimawoods/watchlog/constants/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.nimawoods.watchlog.constants
+
+class AppConstants {
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val api_key = "APIKEY"
+    }
+}
